fc8f3ddb9de1c4419a7cd23f17e7749d5f1d8108
prasaini <sainiprashant060@gmail.com>
2023-02-27 19:53:42 +0530

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,13 @@
+def insertion(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while(j>=0 and arr[j]>key):
+            arr[j+1]=arr[j]
+            j=j-1
+        arr[j+1]=key
+    return arr
+
+
+array=[5,3,2,1,4]
+print(insertion(array))
